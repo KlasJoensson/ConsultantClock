c92f452898487bd2597930e8f42413e629e94ace
klasjonsson <klasjonsson@192.168.88.196>
2018-11-23 09:25:55 +0100

    Staring...

---
+++ src/com/xlent/consultClock/Clock.java
@@ -0,0 +1,14 @@
+package com.xlent.consultClock;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Clock extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
