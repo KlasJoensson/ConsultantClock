b0462d96275129d13f82e93f5f88399a9bb47a78
klasjonsson <klasjonsson@192.168.88.196>
2018-11-30 15:53:22 +0100

    Adds documentation to the Project-class

--- src/com/xlent/consultClock/Project.java
+++ src/com/xlent/consultClock/Project.java
@@ -14,10 +14,22 @@ public class Project {
 	private boolean countDown;
 	private int startTime;
 	
+	/**
+	 * Creates a standard project: The time starts from zero and counting upwards.
+	 * 
+	 * @param name The name of the project
+	 */
 	public Project(String name) {
 		this(name, 0, false);
 	}
 
+	/**
+	 * Creates a generic project with specified parameters.
+	 * 
+	 * @param name The name of the project
+	 * @param time The start time of the project
+	 * @param countDown If it shall count down ({@code true}) or up wards ({@code false})
+	 */
 	public Project(String name, int time, boolean countDown) {
 		this.name = name;
 		this.ticking = false;
@@ -26,10 +38,20 @@ public class Project {
 		this.startTime = time;
 	}
 	
+	/**
+	 * Returns the name of the project.
+	 * 
+	 * @return The project name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Starts counting the time.
+	 * 
+	 * @param timeLabel The label displaying the time
+	 */
 	public void start(Text timeLabel) {
 		ticking = true;
 		timer = new Timer();
@@ -47,27 +69,51 @@ public class Project {
 		}, 0, 1000);
 	}
 	
+	/**
+	 * Pauses the counting.
+	 */
 	public void pause() {
 		ticking = false;
 		timer.cancel();
 	}
 	
+	/**
+	 * Check if the project is counting or not.
+	 * 
+	 * @return {@code true} if the project is counting
+	 */
 	public boolean isTicking() {
 		return ticking;
 	}
 	
+	/**
+	 * Get the current time of the project.
+	 * 
+	 * @return The time in seconds
+	 */
 	public int getTime() {
 		return time;
 	}
 	
+	/**
+	 * Check if the project is set for counting upwards or downwards.
+	 * 
+	 * @return {@code true} if the project is counting down
+	 */
 	public boolean isCountingDown() {
 		return countDown;
 	}
 	
+	/**
+	 * Add one second to the project timer.
+	 */
 	public void addTime() {
 		time++;
 	}
 	
+	/**
+	 * Removes one second to the project timer, if it reaches zero it stops the timer.
+	 */
 	public void countDown( ) {
 		time--;
 		if (time == 0) {
@@ -76,10 +122,18 @@ public class Project {
 		}
 	}
 	
+	/**
+	 * Sets the time to the time used when the project was initiated, i.e. the start time.
+	 */
 	public void resetTime() {
 		time = startTime;
 	}
 	
+	/**
+	 * Get the time as at string on the format hh:mm:ss.
+	 * 
+	 * @return The time on the format hh:mm:ss
+	 */
 	public String getTimeAsString() {
 		StringBuilder timeStr = new StringBuilder();
 		int rest = time%3600;
@@ -101,6 +155,10 @@ public class Project {
 		return timeStr.toString();
 	}
 	
+	/**
+	 * Creates a {@code String} of the data in project. 
+	 */
+	@Override
 	public String toString() {
 		StringBuilder projectStr = new StringBuilder();
 		projectStr.append(getName());
